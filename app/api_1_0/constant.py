2440dbb1e036123bdbf4d47e1d981c02accdb87d
Jack <me@jarrekk.com>
2017-03-30 15:56:58 +0800

    change jack003 to jarrekk

--- app/api_1_0/constant.py
+++ app/api_1_0/constant.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 # author: Kun Jia
 # date: 2/23/17
-# email: me@jack003.com
+# email: me@jarrekk.com
 
 login_required_list = ['get_token']
--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # author: Kun Jia
 # date: 2/23/17
-# email: me@jack003.com
+# email: me@jarrekk.com
 from flask import jsonify
 
 from . import main
@@ -10,4 +10,4 @@ from . import main
 
 @main.route('/', methods=['GET'])
 def hello_world():
-    return jsonify(status='success', message='welcome to jack003!')
+    return jsonify(status='success', message='welcome to jarrekk!')
