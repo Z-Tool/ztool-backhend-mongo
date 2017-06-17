60362ec80f3fe03ebb38dd94a896eeaef11e290d
Jack <me@jarrekk.com>
2017-06-17 10:55:29 +0800

    change decorators to py3.6

--- app/decorators.py
+++ app/decorators.py
@@ -12,11 +12,12 @@ def jsonp(f):
     def decorated_function(*args, **kwargs):
         callback = request.args.get('callback', None)
         rtn = f(*args, **kwargs)
+        print('xxx')
         if isinstance(rtn, tuple):
             content = '{0}({1})'.format(str(callback), rtn[0].data) if callback else rtn[0].data
             status = rtn[1]
         else:
-            content = '{0}({1})'.format(str(callback), rtn.data) if callback else rtn.data
+            content = '{0}({1})'.format(str(callback), eval(rtn.data)) if callback else eval(rtn.data)
             status = 200
         return current_app.response_class(content, mimetype='application/json', status=status)
 
