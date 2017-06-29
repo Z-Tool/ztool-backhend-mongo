d0dd5b5fbe296e874d22990ce0e22d8d741fbed0
Jack <me@jarrekk.com>
2017-06-29 11:29:19 +0800

    add coverage

--- manage.py
+++ manage.py
@@ -10,13 +10,6 @@ from flask_script import Manager, Shell
 from app import create_app, db
 from app.models import Jalpc_pv_count, User
 
-COV = None
-if os.environ.get('FLASK_COVERAGE'):
-    import coverage
-
-    COV = coverage.coverage(branch=True, include='app/*')
-    COV.start()
-
 if os.environ.get('FLASK_CONFIG') == 'prod':
     from gevent import monkey
 
@@ -30,30 +23,7 @@ manager = Manager(app)
 def make_shell_context():
     return dict(app=app, db=db, Jalpc=Jalpc_pv_count, User=User)
 
-
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
-@manager.command
-def test(coverage=False):
-    """Run the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE'):
-        import sys
-        os.environ['FLASK_COVERAGE'] = '1'
-        os.execvp(sys.executable, [sys.executable] + sys.argv)
-    import unittest
-    tests = unittest.TestLoader().discover('app')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
 
 
 if __name__ == '__main__':
