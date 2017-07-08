73e355842911a251ed2cd8942c495f397a5d4900
Jack <me@jarrekk.com>
2017-07-08 18:15:43 +0800

    edit test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-fab rebuild:flask && fab up:flask
-fab rebuild:celery && fab up:celery
+YELLOW='\033[1;33m'
+NC='\033[0m'
+SERVER='root@vps.jarrekk.com'
+ALL=$(ssh $SERVER docker ps |grep nginx | wc -l)
+
+if [ $ALL -eq 0 ];then
+    echo "${YELLOW}Now deploy all docker container...${NC}"
+    fab rebuild && fab up
+else
+    echo "${YELLOW}Now deploy flask & celery...${NC}"
+    fab rebuild:flask && fab up:flask
+    fab rebuild:celery && fab up:celery
+fi
